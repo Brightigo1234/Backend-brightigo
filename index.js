c4eac209603589b4b73b149c356582a07ebab21f
Brightigo1234 <80676023+Brightigo1234@users.noreply.github.com>
2022-01-31 10:59:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to mongo DB 🗃'))
+  .catch((err) => {
+    console.log(err);
+  });
+
+//Load all routes
+const authRouter = require('./routes/auth.routes');
+
+//app.use('/', express.static('dist'));
+
+app.use('/api', authRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Listening on port ${process.env.PORT || 8080}!`);
+});
