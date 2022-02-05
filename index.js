79fb83b38d6d684cbd1ab5bbe915553021ae33b4
Irfan Asif <hashtag.irfan@gmail.com>
2022-02-05 14:13:03 +0530

    added getall route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const User = require('./model/auth.model');
 
 const app = express();
 app.use(cors());
@@ -23,7 +24,11 @@ mongoose
 const authRouter = require('./routes/auth.routes');
 const userRouter = require('./routes/user.routes');
 
-app.get('/', (req,res)=> res.json("Server is running"));
+app.get('/', (req, res) => res.json('Server is running'));
+app.get('/api/allUsers', async (req, res) => {
+  const allUsers = await User.find();
+  res.json(allUsers);
+});
 app.use('/api', authRouter);
 app.use('/api', userRouter);
 
